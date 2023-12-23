071aec985b6de272975f3be7eb3f4440a4e629f9
DoAnBe <tranvanloi197@gmail.com>
2023-12-24 01:09:18 +0700

    be1

--- view/home.php
+++ view/home.php
@@ -12,7 +12,8 @@
     ?>
 
   </title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
   <link rel="stylesheet" href="../public/styles/styles.css?v=<?php echo time(); ?>">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -39,7 +40,9 @@
         </div>
         <div class="col-sm-9">
           <div class="container-fluid nav-edit d-flex">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+              aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -63,7 +66,8 @@
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#"><i class="fa fa-plus-square-o" aria-hidden="true" data-bs-toggle="modal" data-bs-target="#addQuestion"></i>
+                  <a class="nav-link" href="#"><i class="fa fa-plus-square-o" aria-hidden="true" data-bs-toggle="modal"
+                      data-bs-target="#addQuestion"></i>
                   </a>
                 </li>
               </ul>
@@ -78,9 +82,10 @@
           <!-- Sign in, sign up -->
           <?php
           if (isset($_SESSION["username"])) {
-          ?>
+            ?>
             <div id="nav-login">
-              <img src="../public/avatar/<?php echo $_SESSION["avt"]; ?>" style="width: 20px; height: 20px; border-radius: 50%" alt="">
+              <img src="../public/avatar/<?php echo $_SESSION["avt"]; ?>"
+                style="width: 20px; height: 20px; border-radius: 50%" alt="">
               <button type="button" class="btn btn-outline-purple" data-bs-toggle="modal" data-bs-target="">
                 <?php
                 echo $_SESSION["username"];
@@ -90,14 +95,16 @@
                 <a href="logout.php" style="text-decoration: none;">Log out</a>
               </button>
             </div>
-          <?php
+            <?php
           } else {
-          ?>
+            ?>
             <div id="nav-login">
-              <button type="button" class="btn btn-outline-purple" data-bs-toggle="modal" data-bs-target="#sign-in">Sign in</button>
-              <button type="button" class="btn btn-outline-blue" data-bs-toggle="modal" data-bs-target="#sign-up">Sign up</button>
+              <button type="button" class="btn btn-outline-purple" data-bs-toggle="modal" data-bs-target="#sign-in">Sign
+                in</button>
+              <button type="button" class="btn btn-outline-blue" data-bs-toggle="modal" data-bs-target="#sign-up">Sign
+                up</button>
             </div>
-          <?php
+            <?php
           }
 
 
@@ -149,18 +156,21 @@
 
               <div class="mb-3 px-2">
                 <label for="username" class="form-label text-purple">Username</label>
-                <input type="text" class="form-control" id="username_login" name="username_login" placeholder="Username">
+                <input type="text" class="form-control" id="username_login" name="username_login"
+                  placeholder="Username">
                 <div id="error_username_login" style="color: red;"></div>
 
               </div>
               <div class="mb-3 px-2">
                 <label for="password" class="form-label text-purple">Password</label>
-                <input type="password" class="form-control" id="password_login" name="password_login" placeholder="Password">
+                <input type="password" class="form-control" id="password_login" name="password_login"
+                  placeholder="Password">
                 <div id="error_password_login" style="color: red;"></div>
 
               </div>
               <div class="mb-3 px-2">
-                <label for="password" class="form-label">Don't have an account? <a href="#" class="text-blue">Sign up now</a></label>
+                <label for="password" class="form-label">Don't have an account? <a href="#" class="text-blue">Sign up
+                    now</a></label>
               </div>
               <div class="d-flex justify-content-center">
                 <button type="button" class="btn btn-primary btn-bg-purple px-5" id="btn_login">Sign in</button>
@@ -181,7 +191,8 @@
         <div class="modal-body ">
           <!-- <button type="button" class="btn-close text-end" data-bs-dismiss="modal" aria-label="Close"></button>-->
           <div class="d-flex justify-content-center align-items-center">
-            <form class="signin-form p-5 needs-validation" class="my-login-validation" method="POST" novalidate enctype="multipart/form-data">
+            <form class="signin-form p-5 needs-validation" class="my-login-validation" method="POST" novalidate
+              enctype="multipart/form-data">
               <h3 class="text-purple pb-2 text-center">Askany</h3>
 
               <div class="mb-3 px-2" id="error_regis" style="color: red;"></div>
@@ -189,7 +200,8 @@
 
               <div class="mb-3 px-2">
                 <label for="username" class="form-label text-purple">Username</label>
-                <input type="text" class="form-control" id="username_regis" name="username" placeholder="Username" required>
+                <input type="text" class="form-control" id="username_regis" name="username" placeholder="Username"
+                  required>
                 <!-- <div class="invalid-feedback">
                   Please, Enter your username
                 </div> -->
@@ -200,7 +212,8 @@
                 <div class="col-sm">
                   <div class="mb-3 px-2">
                     <label for="lastname" class="form-label text-purple">Last Name</label>
-                    <input type="text" class="form-control" id="lastname_regis" name="lastname" placeholder="last name" required>
+                    <input type="text" class="form-control" id="lastname_regis" name="lastname" placeholder="last name"
+                      required>
                     <div id="error_lastname_regis" style="color: red;"></div>
 
                   </div>
@@ -208,7 +221,8 @@
                 <div class="col-sm">
                   <div class="mb-3 px-2">
                     <label for="firstname" class="form-label text-purple">First Name</label>
-                    <input type="text" class="form-control" id="firstname_regis" name="firstname" placeholder="first name" required>
+                    <input type="text" class="form-control" id="firstname_regis" name="firstname"
+                      placeholder="first name" required>
                     <div id="error_firstname_regis" style="color: red;"></div>
 
                   </div>
@@ -216,13 +230,15 @@
               </div>
               <div class="mb-3 px-2">
                 <label for="password" class="form-label text-purple">Password</label>
-                <input type="password" class="form-control" id="password_regis" name="password" placeholder="Password" required>
+                <input type="password" class="form-control" id="password_regis" name="password" placeholder="Password"
+                  required>
                 <div id="error_password_regis" style="color: red;"></div>
 
               </div>
               <div class="mb-3 px-2">
                 <label for="retype" class="form-label text-purple">Confirm Your Password</label>
-                <input type="password" class="form-control" id="retype_regis" name="retype" placeholder="Confirm your password" required>
+                <input type="password" class="form-control" id="retype_regis" name="retype"
+                  placeholder="Confirm your password" required>
                 <div id="error_retype_regis" style="color: red;"></div>
 
               </div>
@@ -274,10 +290,8 @@
 
 
 
-
-
-
-  <script async src="https://cdn.jsdelivr.net/npm/es-module-shims@1/dist/es-module-shims.min.js" crossorigin="anonymous"></script>
+  <script async src="https://cdn.jsdelivr.net/npm/es-module-shims@1/dist/es-module-shims.min.js"
+    crossorigin="anonymous"></script>
   <script type="importmap">
     {
       "imports": {
